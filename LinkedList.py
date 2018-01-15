57a232f8eebb96996b055264ec2afd1b5b993058
NoraTomas <noratomas@hotmail.com>
2018-01-15 06:47:33 +0100

    advantages and disadvantages of a linked list

--- LinkedList.py
+++ LinkedList.py
@@ -1,1 +1,13 @@
+'''
+Advantages of Linked List:
+- Dynamic sizing (can grow/shrink at runtime)
+- No wasted memory because of dynamic sizing
+- Don't have to shift elements after deletion
+
+Disadvantages of Linked List:
+- Each element takes more space, because it contains an additional pointer
+- Can't access node at position n directly
+- Reverse traversing is difficult
+'''
+
 
