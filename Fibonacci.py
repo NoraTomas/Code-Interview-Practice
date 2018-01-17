dd1e86a458a3df2251d8979607890f04828faac6
Nora Tomas <noratomas@hotmail.com>
2018-01-17 17:11:07 +0100

    fixed Fibonacci edge case

--- Fibonacci.py
+++ Fibonacci.py
@@ -8,8 +8,12 @@ fib(4)  # => 3
 ...
 """
 
+
 def fib(n):
-    if (n == 1 or n == 2):
+    if(n < 0):
+        return 0
+
+    if (n == 0 or n == 1 or n == 2):
         return 1
 
     else:
@@ -19,4 +23,5 @@ print("The 1st Fibonacci number is: " + str(fib(1)))
 print("The 2nd Fibonacci number is: " + str(fib(2)))
 print("The 3rd Fibonacci number is: " + str(fib(3)))
 print("THe 4th Fibonacci number is: " + str(fib(4)))
-print("The 17th Fibonacci number is: " + str(fib(17)))
+print("The 17th Fibonacci number is: " + str(fib(17))) #Supposed to be 1597
+print("The 23rd Fibonacci number is: " + str(fib(23))) #Supposed to be 28657
