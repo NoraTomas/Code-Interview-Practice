7f64fab64c208c763a58e5d9c74c32c1ec3b719c
Nora Tomas <noratomas@hotmail.com>
2018-01-18 11:55:10 +0100

    Optimized Fibonacci to run in constant time

--- Fibonacci.py
+++ Fibonacci.py
@@ -9,19 +9,31 @@ fib(4)  # => 3
 """
 
 
-def fib(n):
-    if(n < 0):
-        return 0
+class FibOptimized(object):
 
-    if (n == 0 or n == 1 or n == 2):
-        return 1
+    def __init__(self):
+        self.memo = {
+            0: 0,
+            1: 1,
+            2: 1
+        }
 
-    else:
-        return fib(n-1) + fib(n-2)
+    def fib(self, n):
+        if n < 0:
+            raise ValueError("Index was negative, no such thing as negative index")
 
-print("The 1st Fibonacci number is: " + str(fib(1)))
-print("The 2nd Fibonacci number is: " + str(fib(2)))
-print("The 3rd Fibonacci number is: " + str(fib(3)))
-print("THe 4th Fibonacci number is: " + str(fib(4)))
-print("The 17th Fibonacci number is: " + str(fib(17))) #Supposed to be 1597
-print("The 23rd Fibonacci number is: " + str(fib(23))) #Supposed to be 28657
+        if n not in self.memo:
+            self.memo[n] = self.fib(n-1) + self.fib(n-2)
+        
+        return self.memo[n]
+
+
+if __name__ == '__main__':
+    f = FibOptimized()
+
+    print("The 1st Fibonacci number is: " + str(f.fib(1)))
+    print("The 2nd Fibonacci number is: " + str(f.fib(2)))
+    print("The 3rd Fibonacci number is: " + str(f.fib(3)))
+    print("The 4th Fibonacci number is: " + str(f.fib(4)))
+    print("The 17th Fibonacci number is: " + str(f.fib(17))) #Supposed to be 1597
+    print("The 23rd Fibonacci number is: " + str(f.fib(23))) #Supposed to be 28657
