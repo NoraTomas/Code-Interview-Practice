2ed6141a55e56f126ec7cbd2d90b43ef6d04bbf8
Nora Tomas <noratomas@hotmail.com>
2018-01-25 17:07:44 +0100

    correct solution to Apple stock problem

--- AppleStocks.py
+++ AppleStocks.py
@@ -10,14 +10,28 @@ Write an efficient function that takes stock_prices_yesterday
 and returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
 """
 
-stock_prices_yesterday = {10, 7, 5, 8, 11, 9}
+stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
 
 def get_max_profit(stock_prices_yesterday):
 
-    minimum_value = min(stock_prices_yesterday)
-    maximum_value = max(stock_prices_yesterday)
+    max_profit = 0
 
-    return maximum_value - minimum_value
+    for outer_price in range(len(stock_prices_yesterday)):
+        for inner_price in range(outer_price+1, len(stock_prices_yesterday)):
+
+            print("Outer price: " + str(outer_price))
+            print("Inner price: " + str(inner_price))
+
+            earlier_price = stock_prices_yesterday[outer_price]
+            later_price = stock_prices_yesterday[inner_price]
+
+
+            potential_max_profit = later_price - earlier_price
+
+            max_profit = max(max_profit, potential_max_profit)
+
+    return max_profit
+
 
 print(get_max_profit(stock_prices_yesterday))
 
